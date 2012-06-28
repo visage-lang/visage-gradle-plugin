3089f087fa9426bc80dcf67c357e28be99e3eb64
Rajmahendra Hegde <rajmahendra@gmail.com>
2012-06-28 21:45:58 +0530

    modified index.html

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -1,7 +1,7 @@
 for DIR in $(find ./repository -type d); do
   (
-      echo "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
+      echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
 		    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
-		     echo  "</pre>\n</body>\n</html>"
+		     echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
 	 done
